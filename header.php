5b0dcc1d67632aacc33c36763f6057b5779103df
rohit <rohitgupta0810@gmail.com>
2016-02-24 09:34:55 -0700

    changin url of header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@ $header = '';
 $header = $header. "<html><head</head><body>";
 $header = $header. "<div align=center><a href=index.php>Story List|</a>";
 $header = $header . "<a href=index.php?s=50>Character List|</a>";
-$header = $header . "<a href=index.php?s=4>Add Characters</a></div><hr>";
+$header = $header . "<a href=add.php?s=4>Add Characters</a></div><hr>";
 $header = $header . "<div align=center>";
 
 echo $header;
